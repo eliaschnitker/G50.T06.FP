fb4f0ddaa81fe8be63cadbc40996d8651814a548
Diego Cristobal <dieg9906@gmail.com>
2021-05-18 23:27:42 +0200

    Parser de Revoke key actualizado

--- src/main/python/secure_all/parser/revoke_json_parser.py
+++ src/main/python/secure_all/parser/revoke_json_parser.py
@@ -5,7 +5,7 @@ from secure_all.parser.json_parser import JsonParser
 class RevokeJsonParser(JsonParser):
     """parser for input key files containing a AccessKey request"""
     #pylint: disable=too-few-public-methods
-    ACCESS_CODE = "AccessCode"
-    DNI = "DNI"
-    MAIL_LIST = "NotificationMail"
-    _key_list =  [ ACCESS_CODE, DNI, MAIL_LIST ]
+    KEY = "Key"
+    REVOCATION = "Revocation"
+    REASON = "Reason"
+    _key_list =  [KEY, REVOCATION, REASON]
